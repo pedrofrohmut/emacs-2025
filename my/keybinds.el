4793a5e53b3eae197cd25fb2985b74d1933e0d25
pedrofrohmut <pedrofrohmut@gmail.com>
2024-10-29 15:51:06 -0300

    Dired helpful keybinds

--- my/keybinds.el
+++ my/keybinds.el
@@ -3,3 +3,9 @@
 (keymap-global-set "M-p" (lambda() (interactive) (scroll-down-command 25)))
 
 (keymap-global-set "C-;" 'comment-region)
+
+;; Dired
+(require 'dired)
+(keymap-set dired-mode-map "b" 'dired-up-directory)
+(keymap-set dired-mode-map "C-c n d" 'dired-create-directory)
+(keymap-set dired-mode-map "C-c n f" 'dired-create-empty-file)
