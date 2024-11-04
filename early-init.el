1dd6280890e63fff7aa4d6aac9317161944a9325
pedrofrohmut <pedrofrohmut@gmail.com>
2024-11-04 14:49:51 -0300

    Updt appearance

--- early-init.el
+++ early-init.el
@@ -1,3 +1,5 @@
+;; Minimal appearance to avoid flash light effect
+(add-to-list 'default-frame-alist '(font . "FiraMono Nerd Font 10"))
 (add-to-list 'default-frame-alist '(alpha-background . 90))
 (add-to-list 'default-frame-alist '(background-color . "#1a1b26"))
 
--- my/appearance.el
+++ my/appearance.el
@@ -1,11 +1,21 @@
-;; Apperance ####################################################################
+;; Appearance ###################################################################
 
-(add-to-list 'default-frame-alist '(font . "FiraMono Nerd Font 11"))
-(add-to-list 'default-frame-alist '(alpha-background . 90))
+;; Fonts
+(set-face-attribute 'default nil :font "FiraMono Nerd Font" :height 100)
 
 ;; Theme and Colors #############################################################
 
 (use-package doom-themes
   :ensure t)
 
-(load-theme 'doom-tokyo-night t)
+(load-theme 'doom-monokai-pro :no-confirm)
+
+;; Tokyo Night Bg
+(set-background-color "#1a1b26")
+
+;; Color for selected text
+(set-face-attribute 'region nil :foreground "#fff" :background "#552")
+
+;; Comments (old value #727072)
+(defvar my-dark-gray-blue "#58a")
+(set-face-foreground 'font-lock-comment-face my-dark-gray-blue)
